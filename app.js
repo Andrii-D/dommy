cf5a1e19c584593e76deb3da64a73c23408f24a9
Andrii-D <dvoyak@gmail.com>
2014-12-23 13:40:47 +0200

    paypal payments and variables

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 mailer = require('express-mailer');
 var logger = require('./source/utils/logger');
+var PAYPAL_BUSINESS = process.env.PAYPAL_BUSINESS|| 'fdgfdghfd@dsvfsd.com';
+var PAYPAL_ENDPOINT = process.env.PAYPAL_ENDPOINT|| 'https://www.paypal.com/cgi-bin/webscr';
+
 var app = express();
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -18,10 +21,12 @@ var path = require('path');
 app.use(cors());
 app.use('/static', express.static(path.join(__dirname, 'source', 'static')));
 app.set('views', path.join(__dirname, 'source', 'static'));
-app.set('view engine', 'jade');
+
 require('./source/health')(app);
 require('./source/posts')(app);
 require('./source/home')(app);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.listen(port, function () {
 	logger.info('Page Rank Checker ' + port + ' ' + env);
