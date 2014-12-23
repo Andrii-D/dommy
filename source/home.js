21767a42a08e4b21f03c82ed86d49673211edf28
Andrii-D <dvoyak@gmail.com>
2014-12-23 13:48:16 +0200

    rendering templates

--- source/home.js
+++ source/home.js
@@ -4,13 +4,15 @@
 var async = require('async');
 var logger = require('./utils/logger');
 var client = require('./utils/redis');
+var PAYPAL_BUSINESS = process.env.PAYPAL_BUSINESS|| 'fdgfdghfd@dsvfsd.com';
+var PAYPAL_ENDPOINT = process.env.PAYPAL_ENDPOINT|| 'https://www.paypal.com/cgi-bin/webscr';
 
 function homepage(app) {
     app.route('/').get(function (req, res, next) {
         res.sendfile(__dirname + '/static/index.html');
     });
     app.route('/payments').get(function (req, res, next) {
-        res.sendfile(__dirname + '/static/payments.html');
+        res.render('payments', { PAYPAL_BUSINESS: PAYPAL_BUSINESS, PAYPAL_ENDPOINT: PAYPAL_ENDPOINT });
     });
 }
 
